fc790491d5627f2859f0fe85a5191b96a268034f
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 11:27:05 +0700

    Update manage.php

--- manage.php
+++ manage.php
@@ -11,29 +11,28 @@ include('config.php');
 	<?php
 	$query = $connection->prepare("SELECT * FROM storedata");
 	$query->execute();
+	$index = 1;
 	?>
 
 	<table>
 		<thead>
 			<tr>
 				<th>No.</th>
-				<th>ID</th>
-				<th>USER</th>
-				<th>PASSWORK</th>
-				<th>EMAIL</th>
-				<th>ANDRESS</th>
+				<th>PRODUCT</th>
+				<th>AMOUNT</th>
+				<th>PROFIT</th>
 				<th colspan="2">Action</th>
 			</tr>
 		</thead>
 	
 		<?php while ($row = $query->fetch(PDO::FETCH_ASSOC)) { ?>
 			<tr>
-				<td><?php echo $row['id']; ?></td>
-				<td><?php echo $row['manager']; ?></td>
-				<td><?php echo $row['pwd']; ?></td>
-				<td><?php echo $row['email']; ?></td>
-				<td><?php echo $row['andress']; ?></td>
+				<td><?php echo $indexx; ?></td>
+				<td><?php echo $row['product']; ?></td>
+				<td><?php echo $row['amount']; ?></td>
+				<td><?php echo $row['profit']; ?></td>
 			</tr>
+			$index+++
 		<?php } ?>
 	</table>
 	<button class="btn" onclick="goBack()">Back</button>
